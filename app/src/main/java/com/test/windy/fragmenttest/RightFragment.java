2bd14a266f3ed146b5b3a33a80d5542179bead78
windog <windiscall@gmail.com>
2016-05-20 21:53:57 +0800

    Fragment  lifecycle

--- app/src/main/java/com/test/windy/fragmenttest/RightFragment.java
+++ app/src/main/java/com/test/windy/fragmenttest/RightFragment.java
@@ -1,8 +1,10 @@
 package com.test.windy.fragmenttest;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,10 +13,74 @@ import android.view.ViewGroup;
  * Created by windog on 2016/5/18.
  */
 public class RightFragment extends Fragment {
+
+    public static final String TAG = "RightFragment";
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        Log.d(TAG,"1.onAttach");
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG,"2.onCreate");
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Log.d(TAG,"3.onCreateView");  //与 Avtivity 的 onCreate() 一样
         View view = inflater.inflate(R.layout.right_fragment,container,false);
         return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Log.d(TAG,"4.onActivityCreated");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "5.onStart");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "6.onResume: ");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "7.onPause: ");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "8.onStop: ");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Log.d(TAG, "9.onDestroyView: ");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "10.onDestroy: ");
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        Log.d(TAG, "11.onDetach: ");
     }
 }
